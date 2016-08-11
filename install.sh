31884d65d121c19f1ef5ade5e366187281fb1d70
pedromreis <pedro.m.reis@gmail.com>
2016-08-11 14:39:21 +0100

    inicial

--- install.sh
+++ install.sh
@@ -13,34 +13,6 @@ WWW_OWNER="www-data"
 SCRIPTS="$MHN_HOME/scripts/"
 cd $SCRIPTS
 
-if [ -f /etc/redhat-release ]; then
-    export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:$PATH
-    #yum updates + health
-    yum clean all -y
-    yum update -y
-
-    #Dump yum info for troubleshooting
-    echo -e "Yum Repo List:\n"
-    yum repolist
-    echo -e "Yum Dev Group Packages:\n"
-    yum grouplist | grep -i development
-    echo -e "Attempting to install Dev Tools"
-    yum groupinfo mark install "Development Tools"
-    yum groupinfo mark convert "Development Tools"
-    yum groupinstall "Development Tools" -y
-    echo -e "Development Tools successfully installed\n"
-
-    WWW_OWNER="nginx"
-    ./install_sqlite.sh
-
-    if [ ! -f /usr/local/bin/python2.7 ]; then
-        echo "[`date`] Installing Python2.7 as a pre-req"
-        ./install_python2.7.sh
-    fi
-
-    ./install_supervisord.sh
-fi
-
 echo "[`date`] Starting Installation of all MHN packages"
 
 echo "[`date`] ========= Installing hpfeeds ========="

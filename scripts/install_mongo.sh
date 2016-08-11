09a94578227def811fe903f74943c32a8444a8f3
pedromreis <pedro.m.reis@gmail.com>
2016-08-11 15:57:41 +0100

    Run 1

--- scripts/install_mongo.sh
+++ scripts/install_mongo.sh
@@ -4,32 +4,28 @@ set -e
 set -x
 
 if [ -f /etc/debian_version ]; then
-    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-    echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
+    #apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+    #echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
+    echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
     apt-get update
     apt-get install -y mongodb-org
+    cat > /etc/systemd/system/mongodb.service <<EOF
+[Unit]
+Description=High-performance, schema-free document-oriented database
+After=network.target
 
-elif [ -f /etc/redhat-release ]; then
-    OS=RHEL
-    export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:$PATH
+[Service]
+User=mongodb
+ExecStart=/usr/bin/mongod --quiet --config /etc/mongod.conf
 
-cat >> /etc/yum.repos.d/mongodb.repo <<EOF
-[mongodb-org]
-name=MongoDB Repository
-baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/
-gpgcheck=0
-enabled=1
+[Install]
+WantedBy=multi-user.target
 EOF
 
-    yum-config-manager --add-repo=/etc/yum.repos.d/mongodb.repo
-    yum -y update
-    mkdir -p /data
-    mkdir -p /data/db
-    yum -y install mongodb-org-server mongodb-org-shell mongodb-org-tools
+systemctl enable mongodb.service
+systemctl start mongodb.service
 
-    if [ ! -f /var/run/mongodb/mongod.pid ]; then
-        /etc/init.d/mongod start
-    fi
 
 else
     echo -e "ERROR: Unknown OS\nExiting!"
